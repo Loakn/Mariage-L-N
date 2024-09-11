9a1ab4980e115cccf2a99ced3a69e68390c9a9fb
Loakn <loryane.aknine@gmail.com>
2024-09-11 11:47:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,18 @@ document.addEventListener('DOMContentLoaded', function() {
     const cardContainer = document.querySelector('.card-container');
     const card = document.getElementById('card');
     const downloadButton = document.getElementById('download-button');
+    const backgroundMusic = document.getElementById('background-music');
     let step = 1;
+
+    // Ajout du bouton pour démarrer la musique
+    document.getElementById('play-music').addEventListener('click', function() {
+        backgroundMusic.play(); // Démarre la musique après le clic
+    });
 
     // Premier clic : retourne la carte (recto -> verso)
     cardContainer.addEventListener('click', function() {
         if (step === 1) {
-          cardContainer.style.transform = 'rotateY(180deg)'; // Effet de retournement 3D
+            cardContainer.style.transform = 'rotateY(180deg)'; // Effet de retournement 3D
             setTimeout(() => {
                 card.src = 'card_verso.png'; // Change l'image pour afficher le verso
                 cardContainer.style.transform = 'rotateY(0deg)'; // Retourner la carte
@@ -35,19 +41,19 @@ document.addEventListener('DOMContentLoaded', function() {
         const pdf = new jsPDF({
             orientation: 'portrait', // Orientation du PDF
             unit: 'px', // Unité en pixels
-            format: [380, 380] // Dimensions des images conservées
+            format: [380,380] // Dimensions des images conservées
         });
 
         // Ajouter la première image (recto)
-        pdf.addImage('card_recto.png', 'PNG', 0, 0, 380, 380);
+        pdf.addImage('card_recto.png', 'PNG', 0, 0, 380,380);
 
         // Ajouter la deuxième image (verso)
         pdf.addPage();
-        pdf.addImage('card_verso.png', 'PNG', 0, 0, 380, 380);
+        pdf.addImage('card_verso.png', 'PNG', 0, 0, 380,380);
 
         // Ajouter la carte Henné
         pdf.addPage();
-        pdf.addImage('henne.png', 'PNG', 0, 0, 380, 380);
+        pdf.addImage('henne.png', 'PNG', 0, 0, 380,380);
 
         // Téléchargement du PDF
         pdf.save('invitation.pdf');
