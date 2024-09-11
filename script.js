834c9523822fbb65dfe6aa085c3faeb30fe83d50
Loakn <loryane.aknine@gmail.com>
2024-09-11 10:00:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,38 @@
 document.addEventListener('DOMContentLoaded', function() {
+    const cardContainer = document.querySelector('.card-container');
     const card = document.getElementById('card');
     const downloadButton = document.getElementById('download-button');
-    const cardContainer = document.querySelector('.card-container');
     let step = 1;
 
-    // Premier clic : retourne la carte (recto -> verso)
     cardContainer.addEventListener('click', function() {
         if (step === 1) {
             cardContainer.style.transform = 'rotateY(180deg)'; // Effet de retournement 3D
             setTimeout(() => {
-                card.src = 'card_verso.png'; // Change l'image pour afficher le verso
-                cardContainer.style.transform = 'rotateY(0deg)'; // Revenir à 0 après l'animation
-            }, 500); // Délai pour l'effet de retournement
+                card.src = 'card_verso.png'; // Afficher le verso
+                cardContainer.style.transform = 'rotateY(0deg)'; // Retourner la carte
+            }, 500); // Délai pour l'animation
             step++;
         } else if (step === 2) {
-            // Deuxième clic : retourne pour afficher la carte Henné
-            cardContainer.style.transform = 'rotateY(180deg)'; // Effet de retournement
+            cardContainer.style.transform = 'rotateY(180deg)'; // Retournement pour la carte Henné
             setTimeout(() => {
-                card.src = 'henne.png'; // Change l'image pour afficher la carte henné
-                cardContainer.style.transform = 'rotateY(0deg)'; // Revenir à 0 après l'animation
+                card.src = 'henne.png'; // Afficher la carte Henné
+                cardContainer.style.transform = 'rotateY(0deg)'; // Retourner à l'avant
             }, 500);
             step++;
         } else if (step === 3) {
-            // Troisième clic : faire disparaître la carte Henné et afficher l'image de téléchargement
-            card.style.display = 'none'; // Cache la carte Henné
-            downloadButton.style.display = 'block'; // Affiche l'image de téléchargement
+            downloadButton.style.display = 'block'; // Affiche le bouton de téléchargement
             step++;
         }
     });
 
-    // Fonction pour générer et télécharger le PDF avec les images
+    // Fonction pour télécharger le PDF
     downloadButton.addEventListener('click', function() {
         const { jsPDF } = window.jspdf;
 
         const pdf = new jsPDF({
-            orientation: 'portrait', // Orientation du PDF
-            unit: 'px', // Unité en pixels
-            format: [1772, 1772] // Dimensions des images conservées
+            orientation: 'portrait',
+            unit: 'px',
+            format: [1772, 1772]
         });
 
         // Ajouter la première image (recto)
